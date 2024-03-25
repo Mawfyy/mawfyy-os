f87f2e47e8438b13fb9562bc616c342073882dc9
Mawfyy <94380448+Mawfyy@users.noreply.github.com>
2024-03-25 12:58:13 -0500

    add: extraConfig option in git

--- home.nix
+++ home.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: 
- {
-
+{pkgs, ...}: {
   imports = [
     ./programs/river
   ];
@@ -13,6 +7,10 @@
     enable = true;
     userName = "Mawfyy";
     userEmail = "94380448+Mawfyy@users.noreply.github.com";
+    extraConfig = {
+        user.signingkey = "94380448+Mawfyy@users.noreply.github.com";
+        commit.gpgsign = true;
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
@@ -21,76 +19,19 @@
     swaybg
     zellij
     bemenu
-
     joshuto
     wineWowPackages.stable
     feh
     just
-
-    # archives
     zip
     xz
     unzip
     p7zip
-
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processer https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
-
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc # it is a calculator for the IPv4/v6 addresses
-
-    # misc
-    cowsay
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
     nix-output-monitor
-
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
-
-    btop # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
-    # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
-
-    spotify
+    glow
+    btop
     xfce.thunar
     lutris
-    alacritty
     grim
     slurp
     wl-clipboard
@@ -100,8 +41,10 @@
     nerdfonts
     iosevka
 
+    brave
+
     (discord.override {
-      withVencord = true;
+      withVencord = false;
       withOpenASAR = true;
     })
   ];
@@ -110,7 +53,10 @@
     enable = true;
     defaultEditor = true;
     settings = {
-      theme = "gruvbox";
+      theme = "everforest_dark";
+      editor = {
+        bufferline = "always";
+      };
     };
     languages = {
       language = [
@@ -120,6 +66,17 @@
         }
         {
           name = "typescript";
+          auto-format = true;
+        }
+        {
+          name = "cpp";
+        }
+        {
+          name = "gleam";
+          auto-format = true;
+        }
+        {
+          name = "nix";
           auto-format = true;
         }
       ];
@@ -133,7 +90,6 @@
         dynamic_padding = true;
         opacity = 0.9;
       };
-      background-opacity = 1.0;
       env.TERM = "xterm-256color";
       font = {
         size = 12;
@@ -151,23 +107,13 @@
           family = "Iosevka";
           style = "Light Italic";
         };
-        draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
     };
   };
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "23.05";
 
-  # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
